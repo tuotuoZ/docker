22ae98ec646417e955eb11ffb087a43e4134b4d8
tuotuoZ <bz15@hampshire.edu>
2018-08-02 13:07:30 -0400

    should get rid of the pre install

--- R/init.R
+++ R/init.R
@@ -2,19 +2,16 @@
 #' A function that install all the librarys
 #'
 #' This function allows you prepare the librarys
-#' @param 
-#' @keywords 
+#' @param
+#' @keywords
 #' @export
 #' @examples
 #' docker_init()
-docker_init <- function(){
-  install.packages('shiny', 'tidyverse', 'dplyr', 'ggplot2' )
-  library('shiny', 'tidyverse', 'ggplot2')
-}
+
 
 library('shiny')
 
-my_ui <- fluidPage( 
+my_ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       textInput(inputId = "names", label = "Baby name(s)"),
@@ -22,7 +19,7 @@ my_ui <- fluidPage(
       checkboxInput("number", "Plot number instead of proportion", F),
       sliderInput(inputId = "year", label = "Year range", val = c(1880, 2014), min = 1880, max = 2014)
     ),
-    
+
     mainPanel(
       textOutput("my_text")
     )
--- R/script.R
+++ R/script.R
@@ -1,7 +1,1 @@
 ## This is a script that will download all the packages and library them.
-
-install.packages('devtools')
-library('devtools')
-
-devtools::install_github('tuotuoZ/docker_test')
-library('docker_test')
